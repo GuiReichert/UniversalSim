3907426d726789ebbd9afba5b0a1bf3fa544f5bf
Guilherme Reichert <guireichert.tr@gmail.com>
2025-06-08 03:00:10 -0300

    Adding Gravitational Forces

--- CelestialBody.cs
+++ CelestialBody.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CelestialBody : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float mass = 0;
+    public float radius = 0;
+    public Vector3 velocity = new Vector3(0, 0, 0); 
+    public Vector3 acceleration = new Vector3(0,0,0); 
+    public bool isStaticBody = false;
+    
 
-    // Update is called once per frame
     void Update()
     {
-        
+        UpdatePosition();
+        UpdateVelocity();        
+    }
+
+    void UpdateVelocity (){
+        velocity += acceleration;
+    }
+
+    void UpdatePosition (){
+        transform.position += new Vector3(velocity.x, 0,velocity.z);
     }
 }
--- GravitySimulator.cs
+++ GravitySimulator.cs
@@ -1,16 +1,42 @@
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GravitySimulator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public List<CelestialBody> bodies;
+    public float G = 0.5f;
     void Update()
     {
-        
+        UpdateBodies();
+    }
+
+    void UpdateBodies(){
+        foreach(var body in bodies){
+            if (body.isStaticBody) continue;
+            Vector3 totalacceleration = Vector3.zero;
+
+            foreach(var differentbody in bodies){
+                if (differentbody == body) continue;
+
+                Vector3 direction = differentbody.transform.position - body.transform.position;
+                float distance = direction.magnitude;
+
+                if (distance == 0f) continue;
+                float accelerationMagnitude = CalculateAcceleration(differentbody.mass, distance);
+                Vector3 acceleration = accelerationMagnitude * direction.normalized;
+
+                totalacceleration += acceleration;  
+            }
+
+            body.acceleration = totalacceleration*0.001f;
+        }
+    }
+
+
+    private float CalculateAcceleration(float diffBodyMass, float distance){
+        return G*diffBodyMass/Mathf.Pow(distance,2);
+
     }
 }
